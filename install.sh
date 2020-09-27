2a89c83cc795b24b787fddbda3803937ad311852
root <root@kunnoh.com>
2020-09-27 16:40:42 +0300

    fixed bugs

--- install.sh
+++ install.sh
@@ -12,18 +12,20 @@ RESET="\033[0m"
 echo "${YELLOW}"*********************************************************
 echo  "${YELLOW}"[*] "${CYAN}"Checking python version installed currently on the system..."${BOLD_GREEN}"
 
-if [ ! hash python ];
+if hash python3;
 then
-    echo ${BOLD_YELLOW}"[!] Warning! ${YELLOW}python not found on the system..."
+    echo ${BOLD_YELLOW}"[*] ${GREEN}${python3 -V} ${YELLOW}is present on the system"
+
+else
+    echo ${BOLD_YELLOW}"[!] Warning! ${YELLOW} python3 not found on the system..."
     echo ${BOLD_GREEN}"[+] ${GREEN}Installing python3 on the system..."
-    exit 1
+
 fi
 
-ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+ver=$(python3 -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
 
 if [ "$ver" -lt "30" ];
 then
-    echo ${BOLD_YELLOW}"[*] ${GREEN}"$(python -V) "is present on the system"
     echo ${BOLD_RED}"[!] ${RED}python3 not found"
     echo ${BOLD_GREEN}"[+] ${GREEN}Installing python3"
     exit 1
@@ -63,10 +65,10 @@ then
 fi
 
 # Installation complete
-echo ${YELLOW}"[*] ${CYAN}Installation Completed Successfully!"
-echo ${YELLOW}"[*] ${CYAN}Type: ${BOLD_GREEN} pysetenv ${CYAN}to use pysetenv"
+echo ${YELLOW}"[*] ${CYAN} Installation Completed Successfully!"
+echo ${YELLOW}"[*] ${CYAN} Type: ${BOLD_GREEN} pysetenv ${CYAN} to use pysetenv"
 
 # Usage
-echo ${GREEN}"Usage: "${BOLD_GREEN}"pysetenv --new VIRTUAL_ENVIRONMENT_NAME "${CYAN}"to create new virtual environment"
-echo ${GREEN}"Usage: "${BOLD_GREEN}"pysetenv VIRTUAL_ENVIRONMENT_NAME ${CYAN}to activate the new virtual environment"
+echo ${GREEN} "Usage: "${BOLD_GREEN} "pysetenv --new VIRTUAL_ENVIRONMENT_NAME "${CYAN}" to create new virtual environment"
+echo ${GREEN} "Usage: "${BOLD_GREEN} "pysetenv VIRTUAL_ENVIRONMENT_NAME ${CYAN} to activate the new virtual environment"
 echo ${YELLOW}"*********************************************************"${RESET}
