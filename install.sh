fcc36010a728efb5b843ac85ef905e7a0185352e
root <root@kunnoh.com>
2020-09-27 19:25:35 +0300

    os check fixed

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ echo -e ${YELLOW}"***********************************************************"${
 if [ -f /etc/os-release ];
 then
     # Get Os details
-    OS_NAME=$(cat /etc/*-release | grep -w NAME | cut -d= -f2 | tr -d '"')
-    OS_VERSION=$(cat /etc/*-release | grep -w VERSION | cut -d= -f2 | tr -d '"')
+    OS_NAME=$(cat /etc/os-release | grep -w NAME | cut -d= -f2 | tr -d '"')
+    OS_VERSION=$(cat /etc/os-release | grep -w VERSION_ID | cut -d= -f2 | tr -d '"')
     echo -e ${BOLD_YELLOW}"[*] ${GREEN}Found:${BOLD_GREEN}" ${OS_NAME} ${GREEN}"Version: "${BOLD_GREEN}${OS_VERSION}
 else
     echo -e ${BOLD_RED}"THIS IS NOT A GNU/LINUX DISTRO"
@@ -44,7 +44,7 @@ ver=$(python3 -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
 if [ "$ver" -lt "30" ];
 then
     echo -e ${BOLD_RED}"[!] ${RED}python3 not found"
-    echo -e ${BOLDGREEN}"[+] ${CYAN}Installing python3"
+    echo -e ${BOLD_GREEN}"[+] ${CYAN}Installing python3"
 fi
 
 echo -e ${BOLD_GREEN}"[+] ${CYAN}Creating directory to hold all Python virtual environments"${RESET}
