ddb7c4ee30e60422949a1be315549bbfabfde0c0
root <root@kunnoh.com>
2020-10-23 17:55:02 +0300

    fixed ubuntu errors

--- install.sh
+++ install.sh
@@ -246,7 +246,7 @@ _install(){
                     sudo add-apt-repository ppa:deadsnakes/ppa -y
                     sudo apt-get update
                     sudo apt-get install -y python${PYSETENV_PYTHON_VERSION}
-                    sudo apt-get install python3.8-distutils -y
+                    sudo apt-get install python${PYSETENV_PYTHON_VERSION}-distutils -y
                     sudo apt-get install python${PYSETENV_PYTHON_VERSION}-dev -y
                     # sudo apt-get install python${PYSETENV_PYTHON_VERSION}-venv -y
                     sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
